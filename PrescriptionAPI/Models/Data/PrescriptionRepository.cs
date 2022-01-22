37c322e4c2d561710c67826d40a13214a2ee97fa
Kakoko <simuzechek@gmail.com>
2022-01-22 14:31:21 +0200

    Feat: Connected our APi to SQL Server Database

---
+++ PrescriptionAPI/Models/Data/PrescriptionRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace PrescriptionAPI.Models.Data
+{
+    public class PrescriptionRepository : IPrescriptionRepository
+    {
+
+        private readonly PrescriptionDataContext _context;
+
+        public PrescriptionRepository(PrescriptionDataContext context)
+        {
+            _context = context;
+        }
+        public void Create(Prescription prescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prescription Get(int id)
+        {
+            return _context.Prescriptions.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Prescription> GetAll()
+        {
+            return _context.Prescriptions.ToList();
+        }
+
+        public void Update(Prescription prescription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PrescriptionAPI/Program.cs
+++ PrescriptionAPI/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using PrescriptionAPI.Models.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+//This is the depedency for the appsettings json
+ConfigurationManager configuration = builder.Configuration;
+
+//Registering our database
+builder.Services.AddDbContext<PrescriptionDataContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-builder.Services.AddScoped<IPrescriptionRepository , InMemoryRepository>();
+builder.Services.AddScoped<IPrescriptionRepository , PrescriptionRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
